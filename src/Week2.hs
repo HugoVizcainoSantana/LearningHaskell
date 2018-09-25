71b9f38a239f54318c194b10dc34ecd98a367b78
HugoVizcainoSantana <hugo.vizcaino.santana@gmail.com>
2018-09-25 16:01:46 +0200

    More exercises of Week 2
    More exercises of Week 2
    
    Removed duplicated file

--- src/Week2.hs
+++ src/Week2.hs
@@ -5,12 +5,24 @@ import Data.Char;
 invertCase::String->String
 invertCase [] = []
 invertCase (x:xs) 
- |isUpper(x) = toLower(x):invertCase(xs)
- |isLower(x) = toUpper(x):invertCase(xs)
- |otherwise = x:invertCase(xs)
+    |isUpper(x) = toLower(x):invertCase(xs)
+    |isLower(x) = toUpper(x):invertCase(xs)
+    |otherwise = x:invertCase(xs)
  
 countUppercaseRecursive::String->Int
 countUppercaseRecursive [] = 0
 countUppercaseRecursive(x:xs)
- |isUpper(x) = 1+ countUppercaseRecursive(xs)
- |otherwise = countUppercaseRecursive(xs)
+    |isUpper(x) = 1+ countUppercaseRecursive(xs)
+    |otherwise = countUppercaseRecursive(xs)
+ 
+ --Exercises week 2 
+
+ --{
+ --   Implement a function receiving a 3-tuple whose elemets are another 2-tuple with elements String and Int
+ --   It should output a 3-tuple containing the first element of each nested 2-tuple
+ --   Inputs :      ((String,Int),(String,Int),(String,Int))
+ --   Outputs:      (String,String,String)
+ --}
+ 
+ejer1::((String,Int),(String,Int),(String,Int))->(String,String,String)
+ejer1 ((s1,_),(s2,_),(s3,_)) = (s1,s2,s3)

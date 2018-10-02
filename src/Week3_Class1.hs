4903e077a073e72ba2303a384b448dbc652e9706
Hugo Vizcaino Santana <hugo.vizcaino.santana@gmail.com>
2018-10-02 16:20:50 +0200

    Learning Lambdas Funcs

--- src/Week3_Class1.hs
+++ src/Week3_Class1.hs
@@ -1,10 +1,12 @@
+module Week3_Class1 where
+
 --{
 -- MergeSort in Haskell
 --}
 mergeSort::[Int]->[Int]
 mergeSort [x] = [x] -- When it only has 1 element stop calling    
-
-mergeSort array = merge(mergeSort (firstHalf array)) (mergeSort (secondHalf array))
+mergeSort array = merge(mergeSort firstHalfArray) (mergeSort secondHalfArray)
+    where (firstHalfArray,secondHalfArray) = splitArray array
     
 merge:: [Int]->[Int]->[Int]
 merge xs [] = xs
@@ -14,8 +16,40 @@ merge (x1:xs) (y1:ys)
     |otherwise  = y1:(merge (x1:xs) ys)
 
 -- Aux funcs
-firstHalf::[Int]->[Int]
-firstHalf array = take (length array `div` 2) array 
+splitArray::[Int]->([Int],[Int])
+splitArray [] = ([],[])
+splitArray array = (take half array,drop half array) 
+    where half = length array `div` 2
 
-secondHalf::[Int]->[Int]
-secondHalf array = drop (length array `div` 2) array
+-- Old Aux Funcs
+-- firstHalf::[Int]->[Int]
+-- firstHalf array = take (length array `div` 2) array 
+--
+-- secondHalf::[Int]->[Int]
+-- secondHalf array = drop (length array `div` 2) array
+
+-- Lambda time
+invertList::[Int]->[Int]
+invertList = foldr (\x lista -> lista++[x]) []
+
+invertList2::[Int]->[Int]
+invertList2 = foldl (\acum x -> x:acum) []
+
+-- Remove all appearances of num in list
+-- In: [1,3,4,12,4,52,35,4,62,6] 4
+-- Out: [1,3,12,52,35,62,6]
+removeNum::[Int]->Int->[Int]
+removeNum array num = foldr(\x acum -> if not(x==num) then x:acum else acum) [] array
+
+-- Insert element at end of list
+insertEnd::[Int]->Int->[Int]
+insertEnd array num = foldr(\x acum->x:acum) [num] array
+
+-- Return # of appearances of element in list
+-- In: [1,2,3,2,4,5,2,6,7] 2
+-- Out: 3
+numOfAppearences::[Int]->Int->Int
+numOfAppearences array num = length (foldr(\x acum -> if (x==num) then x:acum else acum) [] array)
+
+
+-- Functions as first class citizens!

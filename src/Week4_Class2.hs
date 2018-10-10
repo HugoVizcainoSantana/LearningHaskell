057119bd06044084e3ac19cd3b594ff41f44a931
HugoVizcainoSantana <hugo.vizcaino.santana@gmail.com>
2018-10-10 17:42:07 +0200

    Completed custom types exercises

--- src/Week4_Class2.hs
+++ src/Week4_Class2.hs
@@ -11,3 +11,8 @@ move Norte c =Coord (x c) ((y c)+1)
 move Este  c =Coord ((x c)+1) (y c)
 move Sur   c =Coord (x c) ((y c)-1)
 move Oeste c =Coord ((x c)-1) (y c)
+
+distance::Coordenada->Coordenada->Double
+distance (Coord x1 y1) (Coord x2 y2) = sqrt((x1-x2)^2+(y1-y2)^2)
+
+--Recursive Types
